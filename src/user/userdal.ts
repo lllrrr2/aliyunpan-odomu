4116629849070bcdf93411dd7179a0da389288f9
Odomu <1374415225@qq.com>
2023-12-30 21:05:35 +0800

    fix: resource drive load error

--- src/user/userdal.ts
+++ src/user/userdal.ts
@@ -14,6 +14,7 @@ import {
 } from '../store'
 import PanDAL from '../pan/pandal'
 import DebugLog from '../utils/debuglog'
+import { Sleep } from '../utils/format'
 
 export const UserTokenMap = new Map<string, ITokenInfo>()
 
@@ -162,11 +163,20 @@ export default class UserDAL {
       AliUser.ApiUserPic(token),
       AliUser.ApiUserVip(token)
     ])
-    // 保存登录信息
-    await DB.saveValueString('uiDefaultUser', token.user_id)
     useUserStore().userLogin(token.user_id)
-    // 登陆后自动签到
-    await UserDAL.UserAutoSign(token)
+    await Promise.all([
+      // 保存登录信息
+      await DB.saveValueString('uiDefaultUser', token.user_id),
+      // 登陆后自动签到
+      this.UserAutoSign(token),
+      // 刷新Session
+      AliUser.ApiSessionRefreshAccount(token, false),
+      // 刷新OpenApiToken
+      AliUser.OpenApiTokenRefreshAccount(token, false),
+      // 刷新网盘数据
+      PanDAL.aReLoadBackupDrive(token),
+      PanDAL.aReLoadResourceDrive(token)
+    ])
     window.WebUserToken({
       user_id: token.user_id,
       name: token.user_name,
@@ -174,23 +184,16 @@ export default class UserDAL {
       open_api_access_token: token.open_api_access_token,
       login: true
     })
-    // 刷新Session
-    await AliUser.ApiSessionRefreshAccount(token, false)
-    // 刷新OpenApiToken
-    await AliUser.OpenApiTokenRefreshAccount(token, false)
+    await Sleep(1000)
+    await PanDAL.aReLoadOneDirToShow(token.resource_drive_id, 'resource_root', true)
+    await PanDAL.aReLoadOneDirToShow(token.backup_drive_id || token.default_drive_id, 'backup_root', true)
+    PanDAL.aReLoadQuickFile(token.user_id)
     // 刷新所有状态
     useAppStore().resetTab()
     useMyShareStore().$reset()
     useMyFollowingStore().$reset()
     useOtherFollowingStore().$reset()
     useFootStore().mSaveUserInfo(token)
-    // 刷新网盘数据
-    await PanDAL.aReLoadBackupDrive(token)
-    await PanDAL.aReLoadResourceDrive(token)
-    // 展开文件夹
-    await PanDAL.aReLoadOneDirToShow(token.resource_drive_id, 'resource_root', true)
-    await PanDAL.aReLoadOneDirToShow(token.default_drive_id, 'backup_root', true)
-    PanDAL.aReLoadQuickFile(token.user_id)
     message.success('加载用户成功!', 2, loadingKey)
   }
 
