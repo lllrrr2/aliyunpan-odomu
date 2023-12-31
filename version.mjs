1e613d5e02ffb6416a49a38d9e0ac6621a4df953
Odomu <1374415225@qq.com>
2023-12-31 15:35:52 +0800

    chore: update package.json

--- version.mjs
+++ version.mjs
@@ -1,15 +1,18 @@
 import dayjs from 'dayjs'
-import { execSync } from 'child_process'
-import fs from 'fs'
+import { execSync } from 'node:child_process'
+import fs from 'node:fs'
 
 process.env.TZ = 'Asia/Shanghai'
 const packageJsonStr = fs.readFileSync('./package.json').toString()
 try {
+  const tmp = `3.${dayjs().format('YY.MMDDHH')}`
   const packageJson = JSON.parse(packageJsonStr)
-  packageJson.version = `3.${dayjs().format('YY.MMDDHH')}`
-  console.info('版本升级为' + packageJson.version)
-  fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2))
-  execSync(`git add package.json`)
+  if (packageJson.version < tmp) {
+    packageJson.version = tmp
+    console.info('版本升级为' + packageJson.version)
+    fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2))
+    execSync(`git add package.json`)
+  }
 } catch (e) {
   console.error('处理package.json失败，请重试', e.message)
   process.exit(1)
